f6df7f59a46c7d95d34df0ec097e28b842851ebd
allreddy <nithin@enplug.com>
2018-11-10 18:34:50 -0800

    Use react-redux to connect reducer books state to book-list container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        { 'title' : 'Javascript: The good parts'},
+        {'title': 'Harry Potter'},
+        {'title': 'Lord of the rings'},
+        {'title': 'Eloquent Ruby'},
+        {'title': 'Meditations'}
+    ];
+}
+
